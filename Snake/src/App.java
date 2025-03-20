e77f3c2e9ed058b3fd0a5df1ecf64c019909f876
Newton Maluvana <andzanimaluvana@gmail.com>
2025-03-20 12:04:09 +0100

    adding the props to the stage

--- Snake/src/App.java
+++ Snake/src/App.java
@@ -2,17 +2,18 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int GameWidth=800;
+        int GameWidth=1200;
         int GameHeight=800;
 
         JFrame frame =new JFrame("Retro Snake");
         frame.setVisible(true);
-        frame.setSize(GameHeight,GameWidth);
+        frame.setSize(GameWidth,GameHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         Game game=new Game(GameWidth, GameHeight);
         frame.add(game);
+          frame.pack();
     }
 }
--- Snake/src/Game.java
+++ Snake/src/Game.java
@@ -6,16 +6,57 @@ import java.awt.*;
 import javax.swing.*;;
 
 public class Game extends JPanel {
+
+
+
+     private class Tile{
+        int xPosition;
+        int yPosition;
+
+        Tile(int x,int y){
+            xPosition=x;
+            yPosition=y; 
+        }
+     }
+     Tile snakeHead;
+     Tile food;
     int GameHeight;
     int GameWidth;
+    int TileSize=25;
 
 
     Game(int width,int height){
-
+       Random random =new Random();
         this.GameHeight=height;
         this.GameWidth=width;
-        setPreferredSize(new Dimension(GameHeight,GameWidth));
-        setBackground(Color.gray);
+        setPreferredSize(new Dimension(GameWidth,GameHeight));
+        setBackground(Color.black);
+
+        snakeHead=new Tile(5, 5);
+        food =new Tile(random.nextInt(GameWidth),random.nextInt(GameHeight));
+        }
+
+        public void paintComponent(Graphics graphic){
+
+              super.paintComponent(graphic);
+              draw(graphic);
+
+        }
+
+        public void draw(Graphics graphic){
+
+            for(int i=0;i<GameWidth/TileSize;i++){
+
+                graphic.drawLine(i*TileSize, 0, i*TileSize, GameHeight);
+                 graphic.drawLine(0, i*TileSize, GameWidth, i*TileSize);
+            }
+            graphic.setColor(Color.YELLOW);
+            graphic.fillRect(snakeHead.xPosition*TileSize,snakeHead.yPosition*TileSize,TileSize, TileSize);
+
+
+           
+
+
         }
     
 }
